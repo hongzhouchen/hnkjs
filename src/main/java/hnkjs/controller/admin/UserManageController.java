2a3603c70a279db78ebf9d0985d4646a250ed016
hongzhouchen <798838667@qq.com>
2017-12-27 19:23:35 +0800

    login.jsp

--- src/main/java/hnkjs/controller/admin/UserManageController.java
+++ src/main/java/hnkjs/controller/admin/UserManageController.java
@@ -1,31 +1,91 @@
 package hnkjs.controller.admin;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hnkjs.entities.Manager;
+import hnkjs.service.IManagerService;
 
-@RequestMapping("/admin")
+@RequestMapping("admin")
 @Controller
 public class UserManageController {
-
-	@RequestMapping("login")
-	public String login(@RequestParam("loginform") Manager mManager){
-		
+	
+	@Autowired
+	private IManagerService mIManagerService;
+	
+	private static Log log = LogFactory.getLog(UserManageController.class);
+	
+	
+	@RequestMapping(value="login",method=RequestMethod.POST)
+	public String login(String loginName,String password){
+		log.info("loginName:"+loginName+"password:"+password);
 		Subject subject = SecurityUtils.getSubject();
-		if(!subject.isAuthenticated()){//如果没有被认证
-			UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(mManager.getLogin_account(),mManager.getLogin_pass());
+		if(!subject.isAuthenticated()){//如果没有被认证(登录)
+			UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(loginName,password);
 			//记住我
 			usernamePasswordToken.setRememberMe(true);
 			subject.login(usernamePasswordToken);
 		}
-		
 		return "";
 	}
+	
+	/**
+	 * 检测该用户名是否可用
+	 * @param loginName
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="checkLoginName")
+	public String checkLoginName(String loginName){
+		if(loginName.isEmpty()){
+			log.error("检测用户名的时候，传入的用户名为空");
+			return "0:传入的参数为空！";
+		}else{
+			Manager mManager = mIManagerService.queryOneManagerByloginName(loginName);
+			if(mManager!=null){//该账号已经存在
+				return "0:该登录名已经存在！";
+			}else{
+				return "1:该用户名可以使用";
+			}
+		}
+	}
+	
+	/**
+	 * 添加管理员的界面
+	 * @return
+	 */
+	@RequestMapping(value="addManagerPage")
+	public String addManager(ModelMap map){
+		map.addAttribute("title", "添加管理员");
+		map.addAttribute("resultMsg","");
+		return "admin/manager/addManager";
+	}
+	
+	/**
+	 * 添加一个管理员账号
+	 * @param mManager
+	 * @return
+	 */
+	@RequestMapping(value="addManager",method=RequestMethod.POST)
+	public String addManager(Manager mManager,ModelMap map){
+		if(mManager==null){
+		     map.addAttribute("resultMsg","上传的的参数为空");
+		     return "";
+		}
+		return "admin/manager/addManager";
+	}
+	
+	
 	
 	
 }
--- src/main/webapp/WEB-INF/jsp/admin/manager/addManager.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/manager/addManager.jsp
@@ -17,40 +17,63 @@
 <script type="text/javascript" src="layui/layui.all.js"></script>
 </head>
 <body>
-	<blockquote class="layui-elem-quote layui-text">Shiro 登录界面
+	<blockquote class="layui-elem-quote layui-text">添加管理员
 	</blockquote>
 
 
-	<form id="loginform" name="loginform" method="post" action="${pageContext.request.contextPath }/admin/login.action">
+	<form class="layui-form" id="addManagerForm" name="addManagerForm" method="post" action="${pageContext.request.contextPath }/admin/addManager.action">
 		<div class="layui-form-item">
-			<label class="layui-form-label">用户名：</label>
+			<label class="layui-form-label">用户名:</label>
 			<div class="layui-input-block">
-				<input type="text" name="username" lay-verify="title"
+				<input type="text" name="user_name"  lay-verify="required"
 					autocomplete="off" placeholder="邮箱/账号" class="layui-input">
 			</div>
+			<label class="layui-form-label">登录名:</label>
+			<div class="layui-input-block">
+				<input type="text" name="login_account"  lay-verify="required"
+					autocomplete="off" placeholder="邮箱/账号" class="layui-input">
+			</div>
+			
+			
 		</div>
 		<div class="layui-form-item">
 			<label class="layui-form-label">密码：</label>
 			<div class="layui-input-block">
-				<input type="text" name="password" lay-verify="required"
-					placeholder="密码长度为5~15位" autocomplete="off" class="layui-input">
+				<input type="password" name="login_pass" lay-verify="required"
+					placeholder="密码长度为5~15位,默认密码为 888888" autocomplete="off" class="layui-input" value="888888">
 			</div>
 		</div>
 		<div class="layui-form-item">
 			<div class="layui-input-block">
-				<button class="layui-btn" lay-submit="" lay-filter="demo1">立即提交</button>
+				<button class="layui-btn"  lay-filter="">立即提交</button>
 			</div>
 		</div>
+		
+		<div class="layui-form-item">
+		  <c:choose>
+		      <c:when test="${resultMsg}==''">
+		       <span style="color:red">${resultMsg}</span>
+		      </c:when>
+		      <c:otherwise>
+		       <span style="color:#1BC550">${resultMsg}</span>
+		  </c:otherwise>
+		  </c:choose>
+		  
+		</div>
+		
 	</form>
 	<script>
 	layui.use('form', function(){
 		  var form = layui.form; //获取form模块
 		  //监听提交
-		  form.on('submit(loginform)', function(data){
-		    layer.msg(1234);
+		  form.on('submit(addManagerForm)', function(data){
+		    
 		    return true;
 		  });
-		});
+		}
+        	
+	
+	);
 	</script>
 </body>
 </html>
