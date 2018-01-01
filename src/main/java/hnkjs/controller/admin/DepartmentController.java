bf69cd775cb3a23fb46411e7e747ad5bebe49a7e
hongzhouchen <798838667@qq.com>
2018-01-01 18:11:27 +0800

    LOGIN

--- src/main/java/hnkjs/controller/admin/DepartmentController.java
+++ src/main/java/hnkjs/controller/admin/DepartmentController.java
@@ -3,6 +3,7 @@ package hnkjs.controller.admin;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,6 +32,13 @@ public class DepartmentController extends BaseController {
 	    
 		return "";
 	}
+	@RequestMapping("departmentPage")
+	public String departmentPage(ModelMap map){
+		//获取
+		return "";
+	}
+	
+	
 	
 	@RequestMapping("addDepartment")
 	public String addDepartment(Department mDepartment) throws Exception {
